3c1c43de85a3632039652c468f8697acea41a6c9
jmdspeedy <jmdspeedy@users.noreply.github.com>
2026-02-03 10:27:43 +0800

    Fix typescript error in store

--- src/store/config.ts
+++ src/store/config.ts
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
 import { v4 as uuidv4 } from 'uuid';
-import { presetLogos } from '@/utils/logos';
 
 export type ModuleType = 
   | 'Title' | 'Separator' | 'OS' | 'Host' | 'Kernel' | 'Uptime' | 'Packages' | 'Shell' 
