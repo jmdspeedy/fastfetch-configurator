6a9687f2d511bd6ab517e35b7767e59dee6d344e
jmdspeedy <jmdspeedy@users.noreply.github.com>
2026-02-03 10:05:09 +0800

    Update preset logos with authentic ASCII art from fastfetch repo

--- src/utils/logos.ts
+++ src/utils/logos.ts
@@ -1,93 +1,113 @@
 export const presetLogos: Record<string, string> = {
-  "arch": `       /\\
-      /  \\
-     /    \\
-    /      \\
-   /   ,,   \\
-  /   |  |   \\
- /_-''    ''-_\\`,
-  "debian": `       _,met$$$$$gg.
-    ,g$$$$$$$$$$$$$$$P.
-  ,g$$P"     """Y$$.".
- ,$$P'              \`$$$.
-',$$P       ,ggs.     \`$$b:
-\`d$$'     ,$P"'   .    $$$
- $$P      d$'     ,    $$P
- $$:      $$.   -    ,d$$'
- $$;      Y$b._   _,d$P'
- Y$$.    \`.\\"Y$$$$P"'
- \`$$b      "-.__
-  \`Y$$
-   \`Y$$.
-     \`$$b.
-       \`Y$$b.
-          \`"Y$b._
-              \`"""`,
-  "ubuntu": `            .-/+oossssoo+/-.
-        \`:+ssssssssssssssssss+\`
-      -+ssssssssssssssssssyyssss+-
-    .ossssssssssssssssssdMMMNysssso.
-   /ssssssssssshdmmNNmmyNMMMMhssssss/
-  +ssssssssshmydMMMMMMMNddddyssssssss+
- /sssssssshNMMMyhhyyyyhmNMMMNhssssssss/
-.ssssssssdMMMNhsssssssssshNMMMdssssssss.
-+sssshhhyNMMNyssssssssssssyNMMMysssssss+
-ossyNMMMNyMMhsssssssssssssshmmmhssssssso
-ossyNMMMNyMMhsssssssssssssshmmmhssssssso
-+sssshhhyNMMNyssssssssssssyNMMMysssssss+
-.ssssssssdMMMNhsssssssssshNMMMdssssssss.
- \\sssssssshNMMMyhhyyyyhdNMMMNhssssssss/
-  +sssssssssdmydMMMMMMMMddddyssssssss+
-   \\ssssssssssshdmNNNNmyNMMMMhssssss/
-    .ossssssssssssssssssdMMMNysssso.
-      -+ssssssssssssssssssyyssss+-
-        \`:+ssssssssssssssssss+\`
-            .-/+oossssoo+/-.`,
-  "fedora": `          /:-------------:\\
-       :-------------------::
-     :-----------/shhOHbmp---:\\
-   /-----------omMMMNNNMMD  ---:
-  :-----------sMMMMNMNMP.    ---:
- :-----------:MMMdP-------    ---\\
-,------------:MMMd--------    ---:
-:------------:MMMd-------    .---:
-:----    :---:MMMd-------    ---:
-:----    :---:MMMd-------    ---:
-:---.    /---:MMMd-------    ---:
-:---      ---:MMMd-------    ---:
-:---.    /---:MMMd-------    ---:
-:---      ---:MMMd-------    ---:
-:---.    /---:MMMd-------    ---:
-:---      ---:MMMd-------    ---:
-:---.    /---:MMMd-------    ---:
-:---      ---:MMMd-------    ---:`,
-  "windows": `                                ..,
-                    ....,,:;+ccllll
-      ...,,+:;  cllllllllllllllllll
-,cclllllllllll  lllllllllllllllllll
-llllllllllllll  lllllllllllllllllll
-llllllllllllll  lllllllllllllllllll
-llllllllllllll  lllllllllllllllllll
-llllllllllllll  lllllllllllllllllll
-llllllllllllll  lllllllllllllllllll
-                                      
-llllllllllllll  lllllllllllllllllll
-llllllllllllll  lllllllllllllllllll
-llllllllllllll  lllllllllllllllllll
-llllllllllllll  lllllllllllllllllll
-llllllllllllll  lllllllllllllllllll
-\`'ccllllllllll  lllllllllllllllllll
-       \`' \\*::  :ccllllllllllllllll
-                       \`\`''"*::clll`,
-  "apple": `                 .:'
-             __ :'__
-          .'\`  \`-'  \`\`.
-         :          .-'
-         :         :
-          :         \`-;.
-           \`.__.-' .'\`
-.             _.'
- \`--...____...-'`
+  "arch": \`                   -
+                 .o+
+                .ooo/
+               .ooooo:
+              .ooooooo:
+              -+oooooo+:
+            ./:-:++oooo+:
+           ./++++/+++++++:
+          ./++++++++++++++:
+         ./+++ooooooooooooo/
+        ./ooosssso++osssssso+
+       .oossssso-\`\`\`\`/ossssss+
+      -osssssso.      :ssssssso.
+     :osssssss/        osssso+++.
+    /ossssssss/        +ssssooo/-
+  ./ossssso+/:-        -:/+osssso+-
+ .sso+:-                 .-/+oso:
+.++:.                           -/+/
+.                                 /\`,
+  "debian": \`       _,met$$$$$$$$$$gg.
+    ,g$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$P.
+  ,g$$$$P""       """Y$$$$.".
+ ,$$$$P'              \`$$$$$$.
+',$$$$P       ,ggs.     \`$$$$b:
+\`d$$$$'     ,$P"'   .    $$$$$$
+ $$$$P      d$'     ,    $$$$P
+ $$$$:      $$$.   -    ,d$$$$'
+ $$$$;      Y$b._   _,d$P'
+ Y$$$$.    \`.\`"Y$$$$$$$$P"'
+ \`$$$$b      "-.__
+  \`Y$$$$b
+   \`Y$$$$.
+     \`$$$$b.
+       \`Y$$$$b.
+         \`"Y$$b._
+             \`""""\`,
+  "ubuntu": \`                             ....
+              .',:clooo:  .:looooo:.
+           .;looooooooc  .oooooooooo'
+        .;looooool:,''.  :ooooooooooc
+       ;looool;.         'oooooooooo,
+      ;clool'             .cooooooc.  ,,
+         ...                ......  .:oo,
+  .;clol:,.                        .loooo'
+ :ooooooooo,                        'ooool
+'ooooooooooo.                        loooo.
+'ooooooooool                         coooo.
+ ,loooooooc.                        .loooo.
+   .,;;;'.                          ;ooooc
+       ...                         ,ooool.
+    .cooooc.              ..',,'.  .cooo.
+      ;ooooo:.           ;oooooooc.  :l.
+       .coooooc,..      coooooooooo.
+         .:ooooooolc:. .ooooooooooo'
+           .':loooooo;  ,oooooooooc
+               ..';::c'  .;loooo:'\`,
+  "fedora": \`             .',;::::;,'.
+         .';:cccccccccccc:;,.
+      .;cccccccccccccccccccccc;.
+    .:cccccccccccccccccccccccccc:.
+  .;ccccccccccccc;.:dddl:.;ccccccc;.
+ .:ccccccccccccc;OWMKOOXMWd;ccccccc:.
+.:ccccccccccccc;KMMc;cc;xMMc;ccccccc:.
+,cccccccccccccc;MMM.;cc;;WW:;cccccccc,
+:cccccccccccccc;MMM.;cccccccccccccccc:
+:ccccccc;oxOOOo;MMM000k.;cccccccccccc:
+cccccc;0MMKxdd:;MMMkddc.;cccccccccccc;
+ccccc;XMO';cccc;MMM.;cccccccccccccccc'
+ccccc;MMo;ccccc;MMW.;ccccccccccccccc;
+ccccc;0MNc.ccc.xMMd;ccccccccccccccc;
+cccccc;dNMWXXXWM0:;cccccccccccccc:,
+cccccccc;.:odl:.;cccccccccccccc:,.
+ccccccccccccccccccccccccccccc:'.
+:ccccccccccccccccccccccc:;,..
+ ':cccccccccccccccc::;,.\`,
+  "windows": \`/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////
+
+/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////
+/////////////////  /////////////////\`,
+  "apple": \`                     ..'
+                 ,xNMM.
+               .OMMMMo
+               lMM"
+     .;loddo:.  .olloddol;.
+   cKMMMMMMMMMMNWMMMMMMMMMM0:
+ .KMMMMMMMMMMMMMMMMMMMMMMMWd.
+ XMMMMMMMMMMMMMMMMMMMMMMMX.
+;MMMMMMMMMMMMMMMMMMMMMMMM:
+:MMMMMMMMMMMMMMMMMMMMMMMM:
+.MMMMMMMMMMMMMMMMMMMMMMMMX.
+ kMMMMMMMMMMMMMMMMMMMMMMMMWd.
+ 'XMMMMMMMMMMMMMMMMMMMMMMMMMMk
+  'XMMMMMMMMMMMMMMMMMMMMMMMMK.
+    kMMMMMMMMMMMMMMMMMMMMMMd
+     ;KMMMMMMMWXXWMMMMMMMk.
+       "cooc*"    "*coo'"\`
 };
 
 export const getLogoColor = (name: string): string => {
